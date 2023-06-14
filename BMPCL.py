d2546db6c01db7eaa4ba1502350ff7b1ee8627a5
4fantastiques <136469427+4fantastiques@users.noreply.github.com>
2023-06-14 09:00:46 +0200

    Add files via upload

---
+++ BMPCL.py
@@ -0,0 +1,193 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 13 15:56:11 2023
+
+@author: haratono
+"""
+
+import tkinter as tk
+from tkinter import *
+from tkinter import filedialog
+import os
+import pandas as pd
+import numpy as np
+    
+############################## WINDOW ##############################
+root = tk.Tk()
+root.title('BMPCL')
+root.geometry('800x350+20+20')
+root.resizable(0, 0)
+
+   
+############################## FUNCTION ##############################
+
+#================================== directory ==================================
+def rep():
+    print('----------------')
+    full_path = filedialog.askdirectory()
+    
+    #declare directory
+    os.chdir(full_path)
+    print("Répertoire actuel : " + full_path)
+    print('')
+
+def fichier():
+    print('----------------')
+    full_path = filedialog.askopenfilename()
+    print("Chemin : " + full_path)
+    #extract directory of the raw file and insert in the first text box
+    filename = os.path.basename(full_path)
+    TB0.delete("1.0","end")
+    TB0.insert('end', full_path)
+
+    #read raw csv file
+    df = pd.read_csv(full_path, delimiter=",", header=None)
+    
+        
+    #model
+    ep_etalon = TB1.get(1.0, "end-1c") 
+    ep_etalon = float(ep_etalon)
+    larg_etalon= TB2.get(1.0, "end-1c")  
+    larg_etalon = float(larg_etalon)
+
+    #nominal
+    ep_nom = TB3.get(1.0, "end-1c")  
+    ep_nom = float(ep_nom)
+    larg_nom = TB4.get(1.0, "end-1c") 
+    larg_nom = float(larg_nom)
+    
+    #extract every column from dataframe
+    lt = df[0]
+    d1 = df[1]
+    d2 = df[2]
+    d3 = df[3]
+    d4 = df[4]
+    
+    #thickness
+    epai = ep_etalon - d4+d2
+    
+    #width
+    larg = larg_etalon + (d1+d3)
+    
+    new_column = []
+
+    #verification
+    i=0
+    for n in epai:
+        if ((ep_nom-5) < epai[i] < (ep_nom + 5)) and ((larg_nom-5) < larg[i] < (larg_nom + 5)):
+            new_column.insert(i,100)
+        else:
+            new_column.insert(i,0)
+        i=i+1
+        
+    #read plank file
+    df2 = pd.read_csv("planche.csv", delimiter=";", header=None, skiprows=1)
+    planche = df2[0]
+    
+    i1=0
+    i2=1
+    i3=2
+
+    cc = 0
+
+    Number=1
+    Test2=[]
+
+
+    if new_column[0] == 0 :
+        Test2.insert(cc, 0)
+    else:
+        Test2.insert(cc, planche[Number-1])
+        
+    cc=cc+1
+
+
+    for n in range(len(new_column)-2):
+        if new_column[i1] == 0 and new_column[i2] == 0 and new_column[i3] == 0:
+            Test2.insert(cc, 0)
+        elif new_column[i1] == 100 and new_column[i2] == 100 and new_column[i3] == 100:
+            Test2.insert(cc, planche[Number-1])
+        elif new_column[i1] == 0 and new_column[i2] == 100 and new_column[i3] == 100:
+            Test2.insert(cc, planche[Number-1])
+        elif new_column[i1] == 100 and new_column[i2] == 100 and new_column[i3] == 0:
+            Test2.insert(cc, planche[Number-1])
+        elif new_column[i1] == 0 and new_column[i2] == 0 and new_column[i3] == 100:
+            Test2.insert(cc, 0)
+            Number = Number + 1
+        elif new_column[i1] == 100 and new_column[i2] == 0 and new_column[i3] == 100:
+            Test2.insert(cc, 0)
+            Number = Number + 1
+        else:
+            Test2.insert(cc, 0)
+        cc=cc+1
+        i1=i1+1
+        i2=i2+1
+        i3=i3+1
+
+    if new_column[-1] == 0 and new_column[-2]==0:
+        Test2.insert(cc, 0)
+    elif new_column[-1] == 100 and new_column[-2]==0:
+        Test2.insert(cc, 0)
+    elif new_column[-1] == 0 and new_column[-2]==100:
+        Number=Number+1
+        Test2.insert(cc, planche[Number-1])
+    else:
+        Test2.insert(cc, planche[Number-1])
+        
+
+    # m_epai = sum(epai)/len(epai)
+    # m_larg = sum(larg)/len(larg)
+
+    df.insert(5, "epaisseur", epai, True)
+    df.insert(6, "largeur", larg, True)
+    df.insert(7, "verif", Test2, True)
+    
+    # df.insert(7, "Epaisseur moyenne", m_epai, True)
+    # df.insert(8, "Largeur moyenne", m_larg, True)
+
+    # df["Epaisseur moyenne"].loc[1:len(df)] = None
+    # df["Largeur moyenne"].loc[1:len(df)] = None
+
+    df.columns = ['t(s)', 'CH5(mm)', 'CH6(mm)', 'CH7(mm)', 'CH8(mm)', 'epaisseur(mm)','largeur(mm)','numéro du plancher']
+    df.to_csv('num_'+filename, sep=';', decimal=',',header=True, index=False, index_label=None)
+    print('Terminé')
+    print('')
+   
+############################## LABEL ##############################
+L0 = tk.Label(root, text="Chemin du fichier : ").place(x=20, y=20)
+L1 = tk.Label(root, text="Epaisseur de l'étalon (mm) : ").place(x=20, y=60)
+L2 = tk.Label(root, text="Largeur de l'étalon (mm) : ").place(x=20, y=100)
+L3 = tk.Label(root, text="Epaisseur nominale (mm) : ").place(x=20, y=140)
+L4 = tk.Label(root, text="Largeur nominale (mm) : ").place(x=20, y=180)
+
+# ================================== choisir ==================================
+file_button = tk.Button(root, text='Fichier',
+                    bg='red', fg='white', command=fichier)
+file_button.place(x=380, y=260)
+# ================================== repertoire ==================================
+file_button = tk.Button(root, text='Répertoire',
+                    bg='green', fg='white', command=rep)
+file_button.place(x=180, y=260)
+
+############################## TEXT BOX ##############################
+TB0 = tk.Text(root, height=1, width=60)
+TB0.place(x=190, y=20)
+TB1 = tk.Text(root, height=1, width=30)
+TB1.place(x=190, y=60)
+TB2= tk.Text(root, height=1, width=30)
+TB2.place(x=190, y=100)
+TB3 = tk.Text(root, height=1, width=30)
+TB3.place(x=190, y=140)
+TB4 = tk.Text(root, height=1, width=30)
+TB4.place(x=190, y=180)
+
+#insert
+TB1.insert('end', 78.25)
+TB2.insert('end', 110.49) 
+TB3.insert('end', 41)
+TB4.insert('end', 107)
+
+    
+
+
+root.mainloop()
