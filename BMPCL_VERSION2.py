f34d06bf7ff060ae3098742911d89904d2b5f234
AntoineFCBA <135602014+AntoineFCBA@users.noreply.github.com>
2023-08-17 10:19:55 +0200

    Update BMPCL_VERSION2.py
    Formule epaisseur largeur corrigé

--- BMPCL_VERSION2.py
+++ BMPCL_VERSION2.py
@@ -1,178 +1,227 @@
+"""
+Created on Tue Jun 13 15:56:11 2023
 
-# -*- coding: utf-8 -*-
+@author: haratono
+"""
+
 import tkinter as tk
 from tkinter import *
 from tkinter import filedialog
 import os
 import pandas as pd
 import numpy as np
-from openpyxl import Workbook    
-from openpyxl import load_workbook
-import xlrd
-
- ############################## WINDOW ##############################
+    
+############################## WINDOW ##############################
 root = tk.Tk()
 root.title('BMPCL')
-root.geometry('700x350+20+20')
+root.geometry('800x350+20+20')
 root.resizable(0, 0)
-   
 
-# ================================== directory ==================================
-def rep():  
-# directory
+   
+############################## FUNCTION ##############################
+
+#================================== directory ==================================
+def rep():
     print('----------------')
-    full_path = filedialog.askdirectory()  # path of the folder
+    full_path = filedialog.askdirectory()
+    
+    #declare directory
     os.chdir(full_path)
     print("Répertoire actuel : " + full_path)
     print('')
-    
 
 def fichier():
     print('----------------')
-
-    # pour insérer le chemin du fichier
     full_path = filedialog.askopenfilename()
     print("Chemin : " + full_path)
+    #extract directory of the raw file and insert in the first text box
     filename = os.path.basename(full_path)
+    TB0.delete("1.0","end")
     TB0.insert('end', full_path)
-    # -----------------------------------------------
 
-    # read output file avec un délimiteur tabulation
-    df = pd.read_csv(full_path, delimiter=",", header=None, engine='python')
-
-    ep_nom = TB3.get(1.0, "end-1c")  # épaisseur nominale
-    ep_nom = float(ep_nom)
-    larg_nom = TB4.get(1.0, "end-1c")  # largeur nominale
-    larg_nom = float(larg_nom)
+    #read raw csv file
+    df = pd.read_csv(full_path, delimiter=",", header=None)
     
-    ep_etalon = TB1.get(1.0, "end-1c")  # épaisseur nominale
+        
+    #model
+    ep_etalon = TB1.get(1.0, "end-1c") 
     ep_etalon = float(ep_etalon)
-    larg_etalon= TB2.get(1.0, "end-1c")  # largeur nominale
+    larg_etalon= TB2.get(1.0, "end-1c")  
     larg_etalon = float(larg_etalon)
 
+    #nominal
+    ep_nom = TB3.get(1.0, "end-1c")  
+    ep_nom = float(ep_nom)
+    larg_nom = TB4.get(1.0, "end-1c") 
+    larg_nom = float(larg_nom)
+    
+    #extract every column from dataframe
     lt = df[0]
     d1 = df[1]
     d2 = df[2]
     d3 = df[3]
     d4 = df[4]
     
-    # formule pour calculer l'épaisseur
-    epai = ep_etalon - d4 - d2
+    #Epaisseur
+    epai = ep_etalon - d1 - d4
     # formule pour calculer largeur
-    larg = larg_etalon - d1 - d3
+    larg = larg_etalon - d2 - d3
+    
+    fleche = abs(d3)
+    tuil = abs(d4)
+    
+    new_column = []
 
-    #boucle pour vérifier si l'épaisseur est proche de la valeur nominale
+    #verification
     i=0
     for n in epai:
-        if ((ep_nom-2) < epai[i] < (ep_nom + 2)) and ((larg_nom-2) < larg[i] < (larg_nom +2)):
-            epai[i] = epai[i]
-            larg[i] = larg[i]
-           
+        if ((ep_nom-15) < epai[i] < (ep_nom + 15)) and ((larg_nom-15) < larg[i] < (larg_nom + 15)):
+                new_column.insert(i,100)
+                epai[i] = epai[i]
+                larg[i] = larg[i]
         else:
-            epai[i] = np.nan
-            larg[i] = np.nan
-            d1[i]   = np.nan
-            d2[i]   = np.nan
-            d3[i]   = np.nan
-            d4[i]   = np.nan
+                new_column.insert(i,0)
+                epai[i] = np.nan
+                larg[i] = np.nan
+                d1[i]   = np.nan
+                d2[i]   = np.nan
+                d3[i]   = np.nan
+                d4[i]   = np.nan
+                lt[i]   = np.nan
+                fleche[i] = lt[i]
+                tuil[i] = d4[i]
+        i=i+1
+        
+    #read plank file
+    df2 = pd.read_csv("planche.csv", delimiter=";", header=None, skiprows=1)
+    planche = df2[0]
+    
+    i1=0
+    i2=1
+    i3=2
+
+    cc = 0
+
+    Number=1
+    Test2=[]
+
+
+    if new_column[0] == 0 :
+        Test2.insert(cc, 0)
+    else:
+        Test2.insert(cc, planche[Number-1])
+        
+    cc=cc+1
+
+
+    for n in range(len(new_column)-2):
+        if new_column[i1] == 0 and new_column[i2] == 0 and new_column[i3] == 0:
+            Test2.insert(cc, 0)
+        elif new_column[i1] == 100 and new_column[i2] == 100 and new_column[i3] == 100:
+            Test2.insert(cc, planche[Number-1])
+        elif new_column[i1] == 0 and new_column[i2] == 100 and new_column[i3] == 100:
+            Test2.insert(cc, planche[Number-1])
+        elif new_column[i1] == 100 and new_column[i2] == 100 and new_column[i3] == 0:
+            Test2.insert(cc, planche[Number-1])
+        elif new_column[i1] == 0 and new_column[i2] == 0 and new_column[i3] == 100:
+            Test2.insert(cc, 0)
+            Number = Number + 1
+        elif new_column[i1] == 100 and new_column[i2] == 0 and new_column[i3] == 100:
+            Test2.insert(cc, 0)
+            Number = Number + 1
+        else:
+            Test2.insert(cc, 0)
+        cc=cc+1
+        i1=i1+1
+        i2=i2+1
+        i3=i3+1
+
+    if new_column[-1] == 0 and new_column[-2]==0:
+        Test2.insert(cc, 0)
+    elif new_column[-1] == 100 and new_column[-2]==0:
+        Test2.insert(cc, 0)
+    elif new_column[-1] == 0 and new_column[-2]==100:
+        Number=Number+1
+        Test2.insert(cc, planche[Number-1])
+    else:
+        Test2.insert(cc, planche[Number-1])
+        
+        
+    i = 0  
+    for n in Test2 : 
+        if n == 0 :
+            Test2[i] = np.nan
             
-    i=i+1
+            
+        i = i+1 
+    
+    
+    
+
+    
     m_epai = np.nanmean(epai) if len(epai)>0 else np.nan
     m_larg = np.nanmean(larg) if len(larg)>0 else np.nan
-      
+            
+    # m_epai = sum(epai)/len(epai)
+    # m_larg = sum(larg)/len(larg)
 
-    
-    # temp_df = pd.DataFrame({'Epaisseur(mm)':epai,'largeur(mm)':larg})
-    # temp_df.dropna(inplace=True)
-    # epai = temp_df['Epaisseur(mm)'].tolist()
-    # larg =  temp_df['Epaisseur(mm)'].tolist()
-    
     df.insert(5, "epaisseur", epai, True)
     df.insert(6, "largeur", larg, True)
-    df.insert(7, "Epaisseur moyenne", m_epai, True)
-    df.insert(8, "Largeur moyenne", m_larg, True)
+    df.insert(7, "verif", Test2, True)
+    df.insert(8,"tuilage",tuil,True)
+    df.insert(9,"fleche",fleche,True)
+    df.insert(10, "Epaisseur moyenne", m_epai, True)
+    df.insert(11, "Largeur moyenne", m_larg, True)
 
-    df["Epaisseur moyenne"].loc[1:len(df)] = None
-    df["Largeur moyenne"].loc[1:len(df)] = None
+    #df.dropna(thresh=10)
+    ddf = df.dropna()
+    ddf.reset_index(drop = True)
+    
+     
+    # df["Epaisseur moyenne"].loc[1:len(df)] = None
+    # df["Largeur moyenne"].loc[1:len(df)] = None
 
-    df.columns = ['t(s)', 'CH5(mm)', 'CH6(mm)', 'CH7(mm)', 'CH8(mm)', 'Epaisseur(mm)','largeur(mm)', 'Epaisseur moyenne (mm)', 'Largeur moyenne (mm)']
-                  
-    df.to_csv('num_'+filename, sep=';', decimal=',', header=True, index=False, index_label=None)
-             
+    ddf.columns = ['t(s)', 'CH5(mm)', 'CH6(mm)', 'CH7(mm)', 'CH8(mm)', 'epaisseur(mm)','largeur(mm)','numero Erpouvette',"tuilage (mm)","fleche(mm)","Epaisseur moyenne (mm)","Largeur moyenne (mm)"]
+    
+    ddf.to_csv('num_'+filename, sep=';', decimal=',',header=True, index=False, index_label=None)
     print('Terminé')
-    
-def open_empty_excel():
-  workbook = Workbook()
-  sheet = workbook.active
-  sheet.title = "N° Eprouvette"
-        
-        # Sauvegarder le classeur Excel vide
-  empty_excel_path = filedialog.asksaveasfilename(defaultextension='.xlsx')
-  workbook.save(empty_excel_path)
-  workbook.close()
-        
-        # Ouvrir le classeur Excel vide dans l'application par défaut
-  os.system(f'start excel.exe "{empty_excel_path}"')
-  
-def importer():
-    import_file_path = filedialog.askopenfile(filetypes=[("Excel Files","*.csv")])
-    print(import_file_path)
-    if import_file_path:
-        workbook = load_workbook(import_file_path)
-        print(workbook)
-        sheet = workbook.active
-        last_row = sheet.max_row
-        column_data = [sheet.cell(row=i,column=1).value for i in range(1,last_row+1)]
-        csv_file_path = TB0.get(1.0,"end-1c")
-        df = pd.read_csv(csv_file_path,delimier = ";",decimal = ",",header = None)
-        df.insert(10,"Imported Data",column_data)
-      #  df.to_csv('imported_'+os.path.basename(csv_file_path)),sep=';',decimal=',',header = None,index = False)
-        df.to_csv('imported_' + os.path.basename(csv_file_path), sep=';', decimal=',', header=None, index=False)
-        print("Impration termine")
-    
-    ############################## LABEL ##############################
+    print('')
+   
+############################## LABEL ##############################
 L0 = tk.Label(root, text="Chemin du fichier : ").place(x=20, y=20)
-L3 = tk.Label(root, text="Epaisseur de l'etalon (mm) : ").place(x=20, y=60)
-L4 = tk.Label(root, text="Largeur de l'etalon (mm) : ").place(x=20, y=100)
-L1 = tk.Label(root, text="Epaisseur nominale (mm) : ").place(x=20, y=140)
-L2 = tk.Label(root, text="Largeur nominale (mm) : ").place(x=20, y=180)
+L1 = tk.Label(root, text="Epaisseur de l'étalon (mm) : ").place(x=20, y=60)
+L2 = tk.Label(root, text="Largeur de l'étalon (mm) : ").place(x=20, y=100)
+L3 = tk.Label(root, text="Epaisseur nominale (mm) : ").place(x=20, y=140)
+L4 = tk.Label(root, text="Largeur nominale (mm) : ").place(x=20, y=180)
 
 # ================================== choisir ==================================
-file_button = tk.Button(root, text='Choisir fichier',bg='red', fg='white', command=fichier)
-                
-file_button.place(x=70, y=260)
+file_button = tk.Button(root, text='Fichier',
+                    bg='red', fg='white', command=fichier)
+file_button.place(x=380, y=260)
 # ================================== repertoire ==================================
-file_button = tk.Button(root, text='Choisir Repertoire',bg='green', fg='white', command=rep)
-                
-file_button.place(x=240, y=260)
-
-#===================================empty xl==========================
-file_button = tk.Button(root, text='Ouvrir classeur Excel vide', bg='blue', fg='white', command=open_empty_excel)
-file_button.place(x=420, y=260)
-#===================================Importer=========================
-
-file_button = tk.Button(root, text = 'Importer', bg= 'grey', fg='white', command = importer)
-file_button.place(x=595, y= 260)
+file_button = tk.Button(root, text='Répertoire',
+                    bg='green', fg='white', command=rep)
+file_button.place(x=180, y=260)
 
 ############################## TEXT BOX ##############################
+TB0 = tk.Text(root, height=1, width=60)
+TB0.place(x=190, y=20)
 TB1 = tk.Text(root, height=1, width=30)
 TB1.place(x=190, y=60)
-
 TB2= tk.Text(root, height=1, width=30)
 TB2.place(x=190, y=100)
-
 TB3 = tk.Text(root, height=1, width=30)
 TB3.place(x=190, y=140)
-
 TB4 = tk.Text(root, height=1, width=30)
 TB4.place(x=190, y=180)
 
-TB0 = tk.Text(root, height=1, width=60)
-TB0.place(x=190, y=20)
+#insert
+TB1.insert('end', 78.25)
+TB2.insert('end', 110.49) 
+TB3.insert('end', 41)
+TB4.insert('end', 107)
 
-TB3.insert('end', 54)#nominale separeteur de decimale = "."
-TB4.insert('end', 154)
+    
 
-TB1.insert('end', 54.15)
-TB2.insert('end', 156.90) #etalon separateur de décimale = "."
+
 root.mainloop()
